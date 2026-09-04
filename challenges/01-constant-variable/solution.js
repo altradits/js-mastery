005249eae12fe002199155d184d7c6f1368934ae
altradits <altradits@gmail.com>
2026-09-04 21:36:31 +0300

    feat: error handling and pass notification

--- challenges/01-constant-variable/solution.js
+++ challenges/01-constant-variable/solution.js
@@ -1,4 +1,6 @@
 // Challenge 01 - Constant Variable & Export
 //
 // INSTRUCTIONS:
-// Export a constant named `message` with the value "Hello World".
+// Export a constant named `message` 
+// with the value "Hello World".
+export const message = "Hello World";
